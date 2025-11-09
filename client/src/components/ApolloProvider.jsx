4d7a85659b770b7780a56f29db64a5f83da34a5e
Maryam Oladipo <oladipomaryam8@gmail.com>
2025-11-09 21:16:55 +0100

    apolloP

--- client/src/components/ApolloProvider.jsx
+++ client/src/components/ApolloProvider.jsx
@@ -5,15 +5,15 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context';
 
 
-// const httpLink = new HttpLink({
-//   uri: "http://localhost:5000/graphql", // ✅ Usually includes /graphql
-// });
+const httpLink = new HttpLink({
+  uri: "https:merng-server-p8s6.onrender.com/graphql", // ✅ Usually includes /graphql
+});
 
 // Use the deployed backend in production, localhost in development
-const GRAPHQL_URI =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/graphql"
-    : "https://merng-server-p8s6.onrender.com/graphql";
+// const GRAPHQL_URI =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:5000/graphql"
+//     : "https://merng-server-p8s6.onrender.com/graphql";
 
 const authLink = setContext(() => {
   const token = localStorage.getItem('jwtToken')
